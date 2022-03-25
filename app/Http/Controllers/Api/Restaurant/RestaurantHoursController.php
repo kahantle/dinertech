5954ba9eb43b883159b2f94899b5e9f3e570b1b8
deepjani786 <deepj.dotpitchtechnologies@gmail.com>
2022-03-25 17:16:43 +0530

    Restaurant Hours Api Changes add,update,delete api

--- app/Http/Controllers/Api/Restaurant/RestaurantHoursController.php
+++ app/Http/Controllers/Api/Restaurant/RestaurantHoursController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Validator;
 use App\Models\Restaurant;
 use App\Models\RestaurantHours;
+use App\Models\RestaurantHoursTimes;
 use Config;
 use DB;
 class RestaurantHoursController extends Controller
@@ -18,9 +19,10 @@ class RestaurantHoursController extends Controller
 
             $validator = Validator::make($request->all(), [
                 'restaurant_id' => 'required',
-                'opening_time' => 'required',
+                // 'opening_time' => 'required',
                 'days'=>'required',
-                'closing_time' => 'required'
+                'hours' => 'required',
+                // 'closing_time' => 'required'
             ]);
 
             if ($validator->fails()) {
@@ -30,6 +32,8 @@ class RestaurantHoursController extends Controller
             $days='';
             $menuRow =array();
             $requestDays = explode(",",$request->post('days'));
+            $requestHours = $request->post('hours');
+            
             if(is_array($requestDays)){
                 foreach ($requestDays as $key => $value) {
                     $countRestaurantHours = RestaurantHours::where('day',strtolower($value))
@@ -52,18 +56,35 @@ class RestaurantHoursController extends Controller
                                     ->get();
                                    
             $groupId = ($countRestaurantHours)?$countRestaurantHours->count()+1:1;
-            $data=array();
+            // $data=array();
+            // foreach ($requestDays as $key => $value) {
+            //     $data[$key]['restaurant_id'] = $request->post('restaurant_id');
+            //     $data[$key]['hours_group_id'] = $groupId;
+            //     $data[$key]['day'] =  strtolower($value);
+            //     $data[$key]['opening_time'] = $request->post('opening_time');
+            //     $data[$key]['closing_time'] = $request->post('closing_time');
+            // }
+            // $hour = new RestaurantHours;
+            // if($hour->insert($data)){
+            //     return response()->json(['message' => 'Hours added successfully.', 'success' => true], 200);
+            // }
             foreach ($requestDays as $key => $value) {
-                $data[$key]['restaurant_id'] = $request->post('restaurant_id');
-                $data[$key]['hours_group_id'] = $groupId;
-                $data[$key]['day'] =  strtolower($value);
-                $data[$key]['opening_time'] = $request->post('opening_time');
-                $data[$key]['closing_time'] = $request->post('closing_time');
+                $hour = new RestaurantHours;
+                $hour->restaurant_id = $request->post('restaurant_id');
+                $hour->hours_group_id = $groupId;
+                $hour->day            = strtolower($value);
+                $hour->save();
+                foreach($requestHours as $time_key => $time){
+                    $hour_time = new RestaurantHoursTimes;
+                    $hour_time->restaurant_hour_id = $hour->restaurant_hour_id;
+                    $hour_time->hours_group_id = $groupId;
+                    $hour_time->restaurant_id = $request->post('restaurant_id');
+                    $hour_time->opening_time = $time['opening_time'];
+                    $hour_time->closing_time = $time['closing_time'];
+                    $hour_time->save();
+                }
             }
-            $hour = new RestaurantHours;
-            if($hour->insert($data)){
-                return response()->json(['message' => 'Hours added successfully.', 'success' => true], 200);
-            }
+            return response()->json(['message' => 'Hours added successfully.', 'success' => true], 200);
         } catch (\Throwable $th) {
             $errors['success'] = false;
             $errors['message'] = Config::get('constants.COMMON_MESSAGES.CATCH_ERRORS');
@@ -81,10 +102,11 @@ class RestaurantHoursController extends Controller
         try {
             $validator = Validator::make($request->all(), [
                 'restaurant_id' => 'required',
-                'opening_time' => 'required',
+                // 'opening_time' => 'required',
                 'hours_group_id'=>'required',
                 'days'=>'required',
-                'closing_time' => 'required'
+                'hours' => 'required',
+                // 'closing_time' => 'required'
             ]);
             if ($validator->fails()) {
                 return response()->json(['success' => false, 'message' => $validator->errors()], 400);
@@ -93,6 +115,7 @@ class RestaurantHoursController extends Controller
             $days='';
             $menuRow =array();
             $requestDays = explode(",",$request->post('days'));
+            $requestHours = $request->post('hours');
             if(is_array($requestDays)){
                 foreach ($requestDays as $key => $value) {
                     $countRestaurantHours = RestaurantHours::where('day',strtolower($value))
@@ -112,18 +135,36 @@ class RestaurantHoursController extends Controller
             }
 
             RestaurantHours::where('hours_group_id', $request->post('hours_group_id'))->delete();
-            $data=array();
+            RestaurantHoursTimes::where('hours_group_id',$request->post('hours_group_id'))->delete();
             foreach ($requestDays as $key => $value) {
-                $data[$key]['restaurant_id'] = $request->post('restaurant_id');
-                $data[$key]['hours_group_id'] = $request->post('hours_group_id');
-                $data[$key]['day'] =  strtolower($value);
-                $data[$key]['opening_time'] = $request->post('opening_time');
-                $data[$key]['closing_time'] = $request->post('closing_time');
+                $hour = new RestaurantHours;
+                $hour->restaurant_id = $request->post('restaurant_id');
+                $hour->hours_group_id = $request->post('hours_group_id');
+                $hour->day            = strtolower($value);
+                $hour->save();
+                foreach($requestHours as $time_key => $time){
+                    $hour_time = new RestaurantHoursTimes;
+                    $hour_time->restaurant_hour_id = $hour->restaurant_hour_id;
+                    $hour_time->hours_group_id = $request->post('hours_group_id');
+                    $hour_time->restaurant_id = $request->post('restaurant_id');
+                    $hour_time->opening_time = $time['opening_time'];
+                    $hour_time->closing_time = $time['closing_time'];
+                    $hour_time->save();
+                }
             }
-            $hour = new RestaurantHours;
-            if($hour->insert($data)){
-                return response()->json(['message' => 'Hours update successfully.', 'success' => true], 200);
-            }
+            return response()->json(['message' => 'Hours update successfully.', 'success' => true], 200);
+            // $data=array();
+            // foreach ($requestDays as $key => $value) {
+            //     $data[$key]['restaurant_id'] = $request->post('restaurant_id');
+            //     $data[$key]['hours_group_id'] = $request->post('hours_group_id');
+            //     $data[$key]['day'] =  strtolower($value);
+            //     $data[$key]['opening_time'] = $request->post('opening_time');
+            //     $data[$key]['closing_time'] = $request->post('closing_time');
+            // }
+            // $hour = new RestaurantHours;
+            // if($hour->insert($data)){
+            //     return response()->json(['message' => 'Hours update successfully.', 'success' => true], 200);
+            // }
         } catch (\Throwable $th) {
             $errors['success'] = false;
             $errors['message'] = Config::get('constants.COMMON_MESSAGES.CATCH_ERRORS');
@@ -175,6 +216,7 @@ class RestaurantHoursController extends Controller
                 return response()->json(['success' => false, 'message' => $validator->errors()], 400);
             }
             RestaurantHours::where('hours_group_id', $request->post('hours_group_id'))->delete();
+            RestaurantHoursTimes::where('hours_group_id', $request->post('hours_group_id'))->delete();
             return response()->json(['message'=> "Hours delete successfully.",'success' => true], 200);
         } catch (\Throwable $th) {
             $errors['success'] = false;
