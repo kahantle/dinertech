bc08a2b95444488aef2e2b16a5765fc9dd68fe0a
deepjani786 <deepj.dotpitchtechnologies@gmail.com>
2022-06-29 10:46:21 +0530

    seeder changes
    promotion type seeder changes

--- database/seeds/PromotionTypeSeeder.php
+++ database/seeds/PromotionTypeSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\PromotionType;
+use Config;
 
 class PromotionTypeSeeder extends Seeder
 {
@@ -17,61 +18,61 @@ class PromotionTypeSeeder extends Seeder
                 'promotion_name' =>  '% or Discount in Cart',
                 'promotion_details' => '% or Discount in Cart',
                 'promotion_icon'    => null,
-                'status'            => 'ACTIVE',
+                'status'            => Config::get('constants.STATUS.ACTIVE'),
             ],
             [
                 'promotion_name' =>  '% or Discount on Selected items',
                 'promotion_details' => '% or Discount on Selected items',
                 'promotion_icon'    => null,
-                'status'            => 'ACTIVE',
+                'status'            => Config::get('constants.STATUS.ACTIVE'),
             ],
             [
                 'promotion_name' =>  'Free Delivery',
                 'promotion_details' => 'Free Delivery',
                 'promotion_icon'    => null,
-                'status'            => 'ACTIVE',
+                'status'            => Config::get('constants.STATUS.ACTIVE'),
             ],
             [
                 'promotion_name' =>  'Payment Method Reward',
                 'promotion_details' => 'Payment Method Reward',
                 'promotion_icon'    => null,
-                'status'            => 'ACTIVE',
+                'status'            => Config::get('constants.STATUS.ACTIVE'),
             ],
             [
                 'promotion_name' =>  'Get a free Item',
                 'promotion_details' => 'Get a free Item',
                 'promotion_icon'    => null,
-                'status'            => 'ACTIVE',
+                'status'            => Config::get('constants.STATUS.ACTIVE'),
             ],
             [
                 'promotion_name' =>  'buy one get one free',
                 'promotion_details' => 'buy one get one free',
                 'promotion_icon'    => null,
-                'status'            => 'ACTIVE',
+                'status'            => Config::get('constants.STATUS.ACTIVE'),
             ],
             [
                 'promotion_name' =>  'Meal Bundle',
                 'promotion_details' => 'Meal Bundle',
                 'promotion_icon'    => null,
-                'status'            => 'ACTIVE',
+                'status'            => Config::get('constants.STATUS.ACTIVE'),
             ],
             [
                 'promotion_name' =>  'Buy 2,3...get one free',
                 'promotion_details' => 'Buy 2,3...get one free',
                 'promotion_icon'    => null,
-                'status'            => 'ACTIVE',
+                'status'            => Config::get('constants.STATUS.ACTIVE'),
             ],
             [
                 'promotion_name' =>  'Fixed discount amount on a Combo deal',
                 'promotion_details' => 'Fixed discount amount on a Combo deal',
                 'promotion_icon'    => null,
-                'status'            => 'ACTIVE',
+                'status'            => Config::get('constants.STATUS.ACTIVE'),
             ],
             [
                 'promotion_name' =>  '% Discount on combo deal',
                 'promotion_details' => '% Discount on combo deal',
                 'promotion_icon'    => null,
-                'status'            => 'ACTIVE',
+                'status'            => Config::get('constants.STATUS.ACTIVE'),
             ],
         ];
         PromotionType::insert($data);
