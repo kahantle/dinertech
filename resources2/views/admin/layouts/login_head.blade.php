9fe6a0c45938ea67176400e15fe32fa453722b53
jery-dot <jerryp.dotpitchtechnologies@gmail.com>
2024-11-26 19:29:49 +0530

    saving  for 26-11-2024

--- resources2/views/admin/layouts/login_head.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- Bootstrap -->
-<link rel="stylesheet" href="{{asset('assets/admin/css/bootstrap.min.css')}}" />
-<link rel="stylesheet" href="{{asset('assets/admin/css/style-login.css')}}" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
--- resources2/views/customer-layouts/banner.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="banner">
-    <h1>
-        The only thing we're serious about is food.
-    </h1>
-    <a href="#" alt="Start a new Order">Start a new order</a>
-</div>
